e66a333172ec549b6d0f92d8fb461445abbcf3e4
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-10-01 15:29:02 -0700

    Added input activity

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-// grab DOM elements
+// DOM - document object model
+// built into your browser -- its a JS representation of your HTML
 
-// set event listeners
+// DOM GETTER METHODS
+// getElementById
+const nameDisplay = document.getElementById('name-display');
+console.log(nameDisplay);
+
+// DOM SETTER METHODS
+// textContent
+nameDisplay.textContent = 'Andres';
+nameDisplay.classList.add('purple');
+
+const updateBtn = document.getElementById('update-button');
+updateBtn.addEventListener('click'. () => {
+        console.log('i am clicking the button');
+        const nameInput = document.getElementById('name-input');
+        // set display to the value from the input
+        nameDisplay.textContent = // the value from the input
+});
